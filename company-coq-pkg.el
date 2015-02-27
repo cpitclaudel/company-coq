fc70346a5dee0990e4a7acee44acefa2922c0d73
Clément Pit--Claudel <clement.pitclaudel@live.com>
2015-02-27 15:30:17 -0500

    package: Fix the version number for company-math

--- company-coq-pkg.el
+++ company-coq-pkg.el
@@ -1,7 +1,7 @@
 (define-package "company-coq" "0.1"
                 "Company-mode backend for Proof General's coq-mode"
                 '((company "0.8.11")
-                  (company-math "20150216.352")
+                  (company-math "0")
                   (yasnippet "0.9.0.1")
                   (cl-lib "0.5")))
 ;; Local Variables:
