8d8f171bbb765488659bb02a2ffc3fa26e75286d
Clément Pit--Claudel <clement.pitclaudel@live.com>
2016-02-05 21:14:01 -0500

    experiments: Remove hide-all-bullets

--- experiments/hide-all-bullets.el
+++
@@ -1,8 +0,0 @@
-(setq-local hs-allow-nesting t)
-
-(with-current-buffer proof-script-buffer
-  (save-excursion
-    (goto-char (point-max))
-    (while (company-coq-features/code-folding--next-bullet #'re-search-backward)
-      (hs-hide-block-at-point)
-      (goto-char (point-at-bol)))))
