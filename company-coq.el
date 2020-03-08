7a9368febd0d4279454de8506f2822edc2b80343
soraros <soraros@users.noreply.github.com>
2020-03-08 21:53:30 +0100

    Update company-coq.el

--- company-coq.el
+++ company-coq.el
@@ -4263,7 +4263,7 @@ REF-BUFFER is used to retrieve the buffer-local values of
 (defun company-coq-features/prettify-symbols--same-ish-syntax (other ref)
   "Check if the syntax class of OTHER is similar to that of REF."
   (and
-   (not (memq other '(44 46 59 124))) ;; , . ; |
+   (not (memq other '(?. ?, ?\;)))
    (memq (char-syntax (or other ?\s))
          (if (memq (char-syntax ref) '(?w ?_))
              '(?w ?_)
