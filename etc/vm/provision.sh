f9dba9ddff7da99a93d8a6e26d9b1d813bc96b2f
Clément Pit-Claudel <clement.pitclaudel@live.com>
2020-03-13 14:19:26 -0400

    vm: Update Vagrant script

--- etc/vm/provision.sh
+++ etc/vm/provision.sh
@@ -12,10 +12,9 @@ echo '***   Installing base packages   ***'
 echo '************************************'
 
 echo '* apt-get update'
-add-apt-repository -y ppa:ubuntu-elisp/ppa >> /vagrant/provision.log 2>&1
-apt-get -y update >> /vagrant/provision.log 2>&1
+add-apt-repository -y ppa:avsm/ppa >> /vagrant/provision.log 2>&1 # OPAM
 echo '* apt-get install (needs to download about 90MB)'
-apt-get -y install make m4 patch unzip git aspcud ocaml ocaml-native-compilers camlp4-extra opam emacs virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 >> /vagrant/provision.log 2>&1
+apt-get -y install make m4 patch unzip git aspcud camlp4-extra opam emacs virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 >> /vagrant/provision.log 2>&1
 
 sudo su vagrant <<-EOF
 	cat > ~/gui-setup.sh <<-EOS
@@ -38,28 +37,21 @@ sudo su vagrant <<-EOF
 	echo '*** Downloading and building Coq ***'
 	echo '************************************'
 
-	echo '* OPAM upgrade'
-	yes | opam upgrade >> /vagrant/provision.log 2>&1
+	export OPAMYES=true
 	echo '* OPAM init'
-	yes | opam init >> /vagrant/provision.log 2>&1
-	echo '* OPAM repo add'
-	yes | opam repo add coq-core-dev https://coq.inria.fr/opam/core-dev >> /vagrant/provision.log 2>&1
+	opam init --auto-setup --compiler=4.07.0 >> /vagrant/provision.log 2>&1
+	eval \$(opam env)
 	echo '* OPAM update'
-	yes | opam update >> /vagrant/provision.log 2>&1
+	opam update >> /vagrant/provision.log 2>&1
 	echo '* OPAM install (will take a while; maybe 30 minutes)'
-	yes | opam install -j2 coq.8.5~rc1 >> /vagrant/provision.log 2>&1
+	opam install -j2 coq=8.10.2 >> /vagrant/provision.log 2>&1
 
 	echo ""
 	echo '************************************'
 	echo '***   Setting up Proof General   ***'
 	echo '************************************'
 
-	echo '* git clone'
-	rm -rf ~/.emacs.d/lisp/PG
-	git clone https://github.com/ProofGeneral/PG ~/.emacs.d/lisp/PG >> /vagrant/provision.log 2>&1
-	echo '* make'
-	make -C ~/.emacs.d/lisp/PG >> /vagrant/provision.log 2>&1
-
+	mkdir -p ~/.emacs.d/
 	cat > ~/.emacs.d/init.el <<-EOS
 		(add-to-list 'load-path "~/.emacs.d/lisp/PG/generic")
 
@@ -74,7 +66,7 @@ sudo su vagrant <<-EOF
 		(add-hook 'coq-mode-hook #'company-coq-mode)
 
 		;; Terminal keybindings
-		(with-eval-after-load 'coq-mode
+		(with-eval-after-load 'company-coq
 		  (define-key company-coq-map (kbd "C-c RET") #'company-coq-proof-goto-point)
 		  (define-key company-coq-map (kbd "C-c C-j") #'company-coq-proof-goto-point))
 
@@ -90,7 +82,8 @@ sudo su vagrant <<-EOF
 	EOS
 
 	echo '* package install'
-	emacs --batch --load ~/.emacs.d/init.el --eval "(progn (package-refresh-contents) (package-install 'company-coq))" >> /vagrant/provision.log 2>&1
+	emacs --batch --load ~/.emacs.d/init.el \
+		--eval "(progn (package-refresh-contents) (package-install 'proof-general) (package-install 'company-coq))" >> /vagrant/provision.log 2>&1
 
 	echo ""
 	echo '************************************'
